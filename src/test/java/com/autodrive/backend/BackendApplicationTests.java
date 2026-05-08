f6e0873b3ce8238137a92056f645fd739cba4f67
vlad25 <vlad25ichim@gmail.com>
2026-05-08 18:52:35 +0300

    initial-setup

---
+++ src/test/java/com/autodrive/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.autodrive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@ActiveProfiles("test")
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
