7877ad9ecd6a7a1ff2b501919fa1aa417183a247
sabin1603 <sabinbarboi16@gmail.com>
2026-06-24 10:39:19 +0300

    repo for entities

--- src/main/java/com/autodrive/backend/repo/EmployeeRepository.java
+++ src/main/java/com/autodrive/backend/repo/EmployeeRepository.java
@@ -1,13 +1,12 @@
 package com.autodrive.backend.repo;
 
-import com.autodrive.backend.entity.user.Customer;
+import com.autodrive.backend.entity.user.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
     boolean existsByCnp(String cnp);
 
     boolean existsByCnpAndUserIdNot(String cnp, UUID userId);
 }
-
--- src/main/java/com/autodrive/backend/repo/RefreshTokenRepository.java
+++ src/main/java/com/autodrive/backend/repo/RefreshTokenRepository.java
@@ -5,8 +5,9 @@ import com.autodrive.backend.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByToken(String token);
     void deleteByUser(User user);
 }
